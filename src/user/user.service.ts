5973078f96a14f5091c1253cae9a669dfc18400b
Mostafiz002 <mo.mahin4@gmail.com>
2026-04-21 19:53:24 +0600

    feat: add prisma

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,13 @@
+import { PrismaService } from './../prisma.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-  getUserByEmail(email: string) {
-    return {
-      email,
-    };
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getUserByEmail(email: string) {
+    const user = await this.prismaService.user.findFirst({ where: { email } });
+
+    return user;
   }
 }
